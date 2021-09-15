5f0b6586ed843ac96cf61e26ce9bf59913a53813
Terry_Hsiesh <lin020488995@gmail.com>
2021-09-16 00:18:23 +0800

    0916

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 
 	"gopkg.in/yaml.v2"
 )
 
-type GetAllRoomType struct {
+type AllRoomTypeURL struct {
 	Url string `yaml:"URL_RoomType"`
 }
 
@@ -18,21 +19,27 @@ func main() {
 
 func HttpRoomTypeGet() {
 
-	var roomtype GetAllRoomType
-	roomtype.GetAllRoomTypeAPI()
+	var allRoomTypeUrl AllRoomTypeURL
+	allRoomTypeUrl.GetUrl()
+
+	resp, err := http.Get(allRoomTypeUrl.Url)
+
+	Error(err)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	Error(err)
+	fmt.Println(string(body))
 
 }
 
-func (roomtype *GetAllRoomType) GetAllRoomTypeAPI() {
+func (roomtype *AllRoomTypeURL) GetUrl() {
 
 	url, err := ioutil.ReadFile("./url.yaml")
 	Error(err)
-	fmt.Println(url)
 
 	//yaml文件内容影射到结构体中
 	err1 := yaml.Unmarshal(url, &roomtype)
 	Error(err1)
-	fmt.Println(roomtype.Url)
 
 }
 
